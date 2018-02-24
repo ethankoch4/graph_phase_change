6f38bd76730321b187cdbe60d61ace520f730625
Ethan Koch <ethankoch4@gmail.com>
2018-02-24 12:13:39 -0500

    make generate_phase_change.py change walk length depending on sys.argv

--- main/generate_phase_change_plots.py
+++ main/generate_phase_change_plots.py
@@ -1,14 +1,13 @@
 def main():
+    import sys
     from utilities import iterate_out_of_class_probs, save_current_status, plot_save_scores, multiple_sbm_iterate, score_auc
     import warnings
     warnings.filterwarnings('ignore')
     warnings.simplefilter('ignore')
     import numpy as np
 
-    import threading
-
     in_class_probs = [0.8]
-    walk_lengths = [10,20,30,40,50]
+    walk_length = [arg for arg in sys.argv[1:]]
     areas_under_curve = {}
     for walk_length in walk_lengths:
         for in_class_prob in in_class_probs:
@@ -18,8 +17,8 @@ def main():
             stop = 800 # up to, but not including
             step = 8
             walk_length = walk_length
-            num_walks = 50
-            num_nodes = 300
+            num_walks = 20
+            num_nodes = 100
             n_classes = 2
             in_class_prob = in_class_prob
             # out_class_prob = 0.6
@@ -95,7 +94,6 @@ def main():
 
     print(areas_under_curve)
 
-if __name__ == "__main__":
-    print('Beginning to generate phase change plots.')
-    main()
-    print('Script completed generating phase change plots..')
+print('Beginning to generate phase change plots.')
+main()
+print('Script completed generating phase change plots..')
