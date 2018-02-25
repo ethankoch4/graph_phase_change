629f0444117b834875c318885f395055a37669cf
Ethan Koch <ethankoch4@gmail.com>
2018-02-24 19:21:43 -0500

    run on cluster

--- main/run_generate_script.sh
+++ main/run_generate_script.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-echo "RUNNING GENERATE_PLOTS PYTHON SCRIPT FOR:"
-echo "$@"
-python3 generate_phase_change_plots.py "$@"
-cd ..
-git add --all .
-git commit "$*"
-git pull origin master
-git push
+echo "RUNNING GENERATE_PLOTS PYTHON SCRIPT STARTING AT:"
+var=$1
+echo "$1"
+echo "AND ENDING AT:"
+finish=$2
+echo "$2"
+echo "WITH STEP SIZE:"
+step=$3
+echo "$3"
+while [ "$var" -le "$finish" ]
+do
+    echo "$var"
+    COMMAND="python3 generate_phase_change_plots.py "
+    COMMAND+="$var"
+    echo "$COMMAND"
+    sbatch -o "$var".log -t 3-12 --job-name="$var" --wrap="$COMMAND"
+    var=$(($var+$step))
+done
 echo "COMPLETED GENERATE_PLOTS PYTHON SCRIPT"
